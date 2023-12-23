360dffbcc8def986d698dab488840c0a6fb3c385
JMO Devel <jmo.devel@gmail.com>
2023-12-23 13:26:43 +0100

    010 squeaky-clean solved

---
+++ exercism/projects/java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -0,0 +1,29 @@
+class SqueakyClean {
+    private static final int ALPHA = 0x000003B1;
+    private static final int OMEGA = 0x000003C9;
+
+    static String clean(String identifier) {
+        StringBuilder sb = new StringBuilder();
+        boolean camel = false;
+        for( char c : identifier.toCharArray() ){
+            if( c == '-' ){
+                camel = true;
+            } else if( Character.isISOControl( c ) ){
+                sb.append( "CTRL" );
+            } else if( Character.isWhitespace( c ) ){
+                sb.append( "_" );
+            } else if( Character.isLetter( c ) && !isLowerCaseGreek(c) ){
+                if( camel ){
+                    c = Character.toUpperCase(c);
+                    camel = false;
+                }
+                sb.append( Character.toString(c) );
+            }
+        }
+        return sb.toString();
+    }
+    private static boolean isLowerCaseGreek( char c ){
+        return c >= ALPHA && c <= OMEGA;
+    }
+
+}
