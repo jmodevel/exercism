715940160d8f62b82f6979e090a31becfffc5f98
JMO Devel <jmo.devel@gmail.com>
2024-01-15 07:44:19 +0100

    025 armstrong-numbers solved

---
+++ exercism/projects/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+class ArmstrongNumbers {
+
+    boolean isArmstrongNumber(int numberToCheck) {
+        String num = String.valueOf( numberToCheck );
+        return numberToCheck == num.chars()
+                .map( d -> (int) Math.pow(Character.getNumericValue(d), num.length() ) )
+                .sum();
+    }
+
+}
