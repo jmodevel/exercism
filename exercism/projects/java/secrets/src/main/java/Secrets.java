4d902e2525261f31d87e5afcb31e409e685e5590
JMO Devel <jmo.devel@gmail.com>
2024-01-22 07:35:58 +0100

    034 secrets solved

---
+++ exercism/projects/java/secrets/src/main/java/Secrets.java
@@ -0,0 +1,23 @@
+public class Secrets {
+
+    private Secrets() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static int shiftBack(int value, int amount) {
+        return value >>> amount;
+    }
+
+    public static int setBits(int value, int mask) {
+        return value | mask;
+    }
+
+    public static int flipBits(int value, int mask) {
+        return value ^ mask;
+    }
+
+    public static int clearBits(int value, int mask) {
+        return value & ~mask;
+    }
+
+}
