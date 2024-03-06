e6a34261f377da2ac16cdae50f2c3e393b324103
JMO Devel <jmo.devel@gmail.com>
2024-03-06 20:01:13 +0100

    068 allergies solved

---
+++ exercism/projects/java/allergies/src/main/java/Allergies.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Allergies {
+
+    private final int score;
+
+    Allergies(int score) {
+        this.score = score;
+    }
+
+    boolean isAllergicTo(Allergen allergen) {
+        return ( this.score & allergen.getScore() ) == allergen.getScore();
+    }
+
+    List<Allergen> getList() {
+        return Arrays.stream( Allergen.values() )
+                .filter( this::isAllergicTo )
+                .toList();
+    }
+
+}
